327032f22128ff68de34d1a07ffa65f597290245
Andre Seiji Tamanaha <seiji@devnup.com>
2016-11-04 22:16:28 -0200

    Feat: Including SASS files and gulp for SASS compile.

---
+++ app/services/rest.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/*
+ * The angular Rest Client module.
+ */
+angular
+  .module('sade', [])
+  .factory('$Rest', ['$http', '$q',
+
+    function ($http, $q) {
+
+      // Some code here
+
+    }
+
+  ]);
