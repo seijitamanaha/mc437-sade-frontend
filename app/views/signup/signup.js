2d986e5e99e8bb800ef2ae27157bc6c65eb21108
Guilherme Victor <guivictor2011@gmail.com>
2016-11-11 22:15:14 -0200

    pagina de cadastro parcial

--- app/views/signup/signup.js
+++ app/views/signup/signup.js
@@ -2,6 +2,28 @@
 
 angular.module('sade')
 
-.controller('SignupCtrl', [function() {
+.controller('SignupCtrl', ['$scope', function($scope) {
+
+    $scope.step = 1;
+
+    $scope.next = function () {
+        $scope.step += 1;
+    };
+
+    $scope.previous = function () {
+        $scope.step -= 1;
+    };
+
+    $scope.isFirst = function () {
+        return $scope.step == 1;
+    };
+
+    $scope.isLast = function () {
+        return $scope.step == 3;
+    };
+
+    $scope.do_signup = function () {
+        $scope.step = 4;
+    };
 
 }]);
