568dd3b3a5a55747a32a4b12c02e580d27466f0b
Andre Seiji Tamanaha <seiji@devnup.com>
2016-11-11 22:36:08 -0200

    Fix: Running SASS

--- app/views/signup/signup.js
+++ app/views/signup/signup.js
@@ -2,28 +2,43 @@
 
 angular.module('sade')
 
-.controller('SignupCtrl', ['$scope', function($scope) {
+  .controller('SignupCtrl', ['$scope', function ($scope) {
 
     $scope.step = 1;
 
     $scope.next = function () {
-        $scope.step += 1;
+      $scope.step += 1;
     };
 
     $scope.previous = function () {
-        $scope.step -= 1;
+      $scope.step -= 1;
     };
 
     $scope.isFirst = function () {
-        return $scope.step == 1;
+      return $scope.step == 1;
     };
 
     $scope.isLast = function () {
-        return $scope.step == 3;
+      return $scope.step == 3;
     };
 
     $scope.do_signup = function () {
-        $scope.step = 4;
+      $scope.step = 4;
     };
 
-}]);
+    $scope.skills = [
+      {
+        id: 1,
+        name: 'Java'
+      },
+      {
+        id: 2,
+        name: 'Java'
+      },
+      {
+        id: 3,
+        name: 'Java'
+      }
+    ];
+
+  }]);
