710aba4b29e24f2242c9e64e7e928b25c98438f8
Guilherme Victor <guivictor2011@gmail.com>
2016-12-02 21:11:34 -0200

    Tela de perfil

--- app/views/profile/profile.js
+++ app/views/profile/profile.js
@@ -2,72 +2,107 @@
 
 angular.module('sade')
 
-  .controller('ProfileCtrl', ['$scope', '$User', function ($scope, $User) {
+    .controller('ProfileCtrl', ['$scope', '$User', function ($scope, $User) {
 
-    $scope.input = {};
+        $scope.PROFILE = 1;
+        $scope.SKILLS = 2;
 
-    $scope.step = 1;
-    $scope.message = '';
-    $scope.loading = false;
+        $scope.input = {};
 
-    $scope.next = function () {
-      $scope.step += 1;
-    };
+        $scope.tab = 1;
+        $scope.editMode = false;
 
-    $scope.previous = function () {
-      $scope.step -= 1;
-    };
-
-    $scope.isFirst = function () {
-      return $scope.step == 1;
-    };
-
-    $scope.isLast = function () {
-      return $scope.step == 3;
-    };
-
-    $scope.do_signup = function (input) {
-
-      $scope.message = '';
-      $scope.loading = true;
-
-      var data = {
-        name: input.name,
-        mail: input.email,
-        cpf: input.cpf,
-        password: input.senha
-      };
-
-      $User.signup(data).then(function() {
+        $scope.message = '';
         $scope.loading = false;
-        $scope.step = 4;
-      }).catch(function () {
-        $scope.loading = false;
-        $scope.message = 'Falha ao realizar cadastro';
-      });
-    };
 
-    $scope.skills = [
-      {
-        id: 1,
-        name: 'C#'
-      },
-      {
-        id: 2,
-        name: 'Java'
-      },
-      {
-        id: 3,
-        name: 'Ruby'
-      }
-    ];
+        $scope.showProfileTab = function () {
+            $scope.tab = $scope.PROFILE;
+            $scope.setEditMode(false);
+        };
 
-    $scope.skills = $scope.skills.map(function(x){
-      return {
-        id: x.id,
-        name: x.name,
-        value: 10
-      }
-    });
+        $scope.showSkillsTab = function () {
+            $scope.tab = $scope.SKILLS;
+            $scope.setEditMode(false);
+        };
 
-  }]);
+        $scope.isProfileTab = function () {
+            return $scope.tab == $scope.PROFILE;
+        };
+
+        $scope.isSkillsTab = function () {
+            return $scope.tab == $scope.SKILLS;
+        };
+
+        $scope.setEditMode = function (b) {
+            $scope.editMode = b;
+
+            // reseta form
+            if (b == false) {
+                $scope.resetForm();
+            }
+        };
+
+        $scope.resetForm = function () {
+            //TODO
+            //$scope.user = angular.copy(originalModel);
+        };
+
+        $scope.isEditMode = function () {
+            return $scope.editMode;
+        };
+
+        $scope.do_update = function (input) {
+
+            $scope.message = '';
+            $scope.loading = true;
+
+            //TODO
+            var data = {
+                name: input.name,
+                mail: input.email,
+                cpf: input.cpf,
+                password: input.senha,
+                rg: input.rg,
+                curriculum: input.curriculo,
+                phone: input.tel,
+                course: input.curso,
+                institution: input.instituicao,
+                address: input.endereco,
+                howMet: input.conheceu,
+                skills: $scope.skills
+            };
+
+            $User.signup(data).then(function () {
+                $scope.loading = false;
+
+            }).catch(function () {
+                $scope.loading = false;
+                $scope.message = 'Falha ao atualizar ' + (($scope.isProfileTab()) ? 'os dados pessoais.' : 'as habilidades.');
+            });
+        };
+
+        // TODO
+        $scope.skills = [
+            {
+                id: 1,
+                name: 'C#'
+            },
+            {
+                id: 2,
+                name: 'Java'
+            },
+            {
+                id: 3,
+                name: 'Ruby'
+            }
+        ];
+
+        $scope.skills = $scope.skills.map(function (x) {
+            return {
+                id: x.id,
+                name: x.name,
+                value: 10
+            }
+        });
+
+    }]);
