15de54f3d75d7c2769274e4b81946916f2b2d77a
Guilherme Victor <guivictor2011@gmail.com>
2016-12-03 01:02:11 -0200

    Atualizacao profile

--- app/views/profile/profile.js
+++ app/views/profile/profile.js
@@ -1,41 +1,30 @@
 'use strict';
 
 angular.module('sade')
-
     .controller('ProfileCtrl', ['$scope', '$User', function ($scope, $User) {
 
-        // TODO
-        var model = {
-            name: 'Andre Seiji',
-            email: 'naofaznada@email.com',
-            cpf: '12312312388',
-            rg: '123123123',
-            curriculum: 'http://www.google.com',
-            phone: '1238123-123',
-            celular: '88888-123',
-            course: 'Enfermage',
-            institution: 'PUC campinas',
-            address: 'campinas 1828',
-            howMet: 'site do saadeee'
-        };
+        var model = {};
 
-        var skills = [
-            {
-                id: 1,
-                name: 'C#',
-                value: 4
-            },
-            {
-                id: 2,
-                name: 'Java',
-                value: 3
-            },
-            {
-                id: 3,
-                name: 'Ruby',
-                value: 9
+        $scope.loading = true;
+        $User.getUser().then(function(response) {
+            $scope.loading = false;
+            console.log(response.object);
+            $scope.user_data = response.object;
+            $scope.skills = $scope.user_data.skills.map(function (x) {
+                return {
+                    id: x.id,
+                    name: x.name,
+                    value: x.score
+                }
+            });
+            $scope.resetForm();
+        }).catch(function(error){
+            if(error.data && error.data.message == 'Usuário deslogado!') {
+                $User.logout();
             }
-        ];
+            $scope.loading = false;
+            console.log(error);
+        });
 
         $scope.PROFILE = 1;
         $scope.SKILLS = 2;
@@ -51,7 +40,7 @@ angular.module('sade')
         $scope.input = {};
 
         $scope.tab = $scope.PROFILE;
-        $scope.editMode = true;
+        $scope.editMode = false;
 
         $scope.message = '';
         $scope.loading = false;
@@ -84,30 +73,18 @@ angular.module('sade')
         };
 
         $scope.resetForm = function () {
-            $scope.skills = skills.map(function (x) {
-                return {
-                    id: x.id,
-                    name: x.name,
-                    value: x.value
-                }
-            });
 
-            $scope.user = {
-                name: model.name,
-                email: model.email,
-                cpf: model.cpf.substr(0, 3) + '.' + model.cpf.substr(3, 3) + '.' + model.cpf.substr(6, 3) + '-' + model.cpf.substr(9),
-            };
+            console.log($scope.user_data);
 
             $scope.input = {
-                rg: model.rg,
-                curriculum: model.curriculum,
-                phone: model.phone,
-                celular: model.celular,
-                course: model.course,
-                institution: model.institution,
-                address: model.address,
-                howMet: model.howMet,
-                skills: $scope.skills
+                rg: $scope.user_data.rg,
+                curriculum: $scope.user_data.curriculum,
+                phone: $scope.user_data.phone,
+                course: $scope.user_data.course,
+                institution: $scope.user_data.institution,
+                address: $scope.user_data.address,
+                howMet: $scope.user_data.howMet,
+                skills: $scope.user_data.skills
             }
         };
 
@@ -132,7 +109,6 @@ angular.module('sade')
                     rg: input.rg,
                     curriculum: input.curriculum,
                     phone: input.phone,
-                    celular: input.celular,
                     course: input.course,
                     institution: input.institution,
                     address: input.address,
@@ -142,14 +118,11 @@ angular.module('sade')
 
                 $User.signup(data).then(function () {
                     $scope.loading = false;
-
                 }).catch(function () {
                     $scope.loading = false;
                     $scope.message = 'Falha ao atualizar ' + (($scope.isProfileTab()) ? 'os dados pessoais.' : 'as habilidades.');
                 });
             }
         };
-
-        $scope.resetForm();
 
     }]);
